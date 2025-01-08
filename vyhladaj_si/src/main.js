f3a549749706070172b7fc4cab9d04096a50bafe
luqeey <eliaslukas557@gmail.com>
2025-01-08 09:07:24 +0100

    teilwind a landing page

--- vyhladaj_si/src/main.js
+++ vyhladaj_si/src/main.js
@@ -1,7 +1,7 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 import router from './router'
 
 const app = createApp(App)
---
+++ vyhladaj_si/src/views/LandingPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-amber-300">Landing NachoVerse</h1>
+  </div>
+</template>
