39a94f62fd3d246d7e5e90b92130b7f4c585d21a
Jakub <kubkozak@gmail.com>
2025-01-20 10:30:30 +0100

    landing page
    svg stale nefunguje

--- backend/vyhladajBE/resources/js/components/NavBar.vue
+++ backend/vyhladajBE/resources/js/components/NavBar.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex items-center w-[28.75rem] justify-between">
-    <router-link to="/" class="text-[#F8E7F8]">
-      <img class="w-52 ml-16" src="@/components/nachoverse-logo.png" alt="">
-    </router-link>
-    <router-link to="/movies" class="text-[#F8E7F8]">Movies</router-link>
-    <router-link to="/tv-series" class="text-[#F8E7F8]">Series</router-link>
+  <div class="bg-[#121212]">
+    <div class="flex items-center w-[28.75rem] justify-between h-[70px] ">
+      <router-link to="/" class="text-[#F8E7F8]">
+        <img class="w-52 ml-16" src="@/components/nachoverse-logo.png" alt="">
+      </router-link>
+      <router-link to="/movies" class="text-[#F8E7F8]">Movies</router-link>
+      <router-link to="/tv-series" class="text-[#F8E7F8]">Series</router-link>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
