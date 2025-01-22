d459911b6787dbb8135d7b4b4df10fb5f20a0bd6
Jakub <kubkozak@gmail.com>
2025-01-22 10:25:29 +0100

    landing page

--- backend/vyhladajBE/resources/js/components/LandingPage.vue
+++ backend/vyhladajBE/resources/js/components/LandingPage.vue
@@ -2,40 +2,16 @@
     <section>
       <div class="relative">
         <div class="flex items-center">
-          <!-- Search Icon SVG -->
-          <span class="absolute inset-y-0 left-4 flex items-center z-20">
-            <svg width="20" height="20" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g filter="url(#filter0_d_28_113)">
-                <path d="M18.5833 15.0833L22.3333 18.8333" stroke="white" stroke-width="1.38889" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M20.6667 9.66666C20.6667 5.52452 17.3089 2.16666 13.1667 2.16666C9.02455 2.16666 5.66669 5.52452 5.66669 9.66666C5.66669 13.8088 9.02455 17.1667 13.1667 17.1667C17.3089 17.1667 20.6667 13.8088 20.6667 9.66666Z" stroke="white" stroke-width="1.38889" stroke-linejoin="round"/>
-              </g>
-              <defs>
-                <filter id="filter0_d_28_113" x="0" y="0.5" width="28" height="28" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                  <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-                  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                  <feOffset dy="4"/>
-                  <feGaussianBlur stdDeviation="2"/>
-                  <feComposite in2="hardAlpha" operator="out"/>
-                  <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_28_113"/>
-                  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_28_113" result="shape"/>
-                </filter>
-              </defs>
-            </svg>
-          </span>
-  
-          <!-- Search Input -->
           <input
             type="search"
             placeholder="Search for movie or series..."
-            class="bg-[#1C1C1C] text-white placeholder-gray-400 rounded-full border border-gray-600 z-30 focus:outline-none pl-12 pr-4 py-3 w-[28rem]"
+            class="bg-[#1C1C1C]/60 backdrop-blur-md text-white placeholder-gray-400 rounded-full border border-gray-600 z-30 focus:outline-none pl-12 pr-4 py-3 w-[28rem]"
           />
         </div>
       </div>
   
       <!-- Dark Fade Effect -->
       <div class="fade-effect"></div>
-  
       <div class="background">
         <div class="grid-wrapper">
           <!-- Each column of images (Max 8 columns) -->
@@ -44,8 +20,14 @@
             :key="i"
             class="scroll-column"
             :style="{
-              animationDuration: '100s',  /* Slow speed: 30 seconds per scroll cycle */
-              animationDirection: i % 2 === 0 ? 'normal' : 'reverse', /* One scrolls up, other down */
+              animationDuration: '30s',  /* Slow speed: 30 seconds per scroll cycle */
+              animationDirection: i % 2 === 0 ? 'normal' : 'reverse', /* One scrolls up, the other down */
+              position: 'absolute',
+              // top: '100', /* All columns start from the top */
+              left: `${(i - 1) * 12.5}%`, /* Position each column horizontally in a grid */
+              width: '12.5%', /* 8 columns should take up 100% of the width, so each gets 12.5% */
+              height: '100vh', /* Ensure the column takes up the entire height */
+              padding: '5px'
             }"
           >
             <img
@@ -172,5 +154,12 @@
     fill: none;
     stroke: white; /* Ensure it's visible against dark background */
   }
+
+  input[type="search"]::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    appearance: none;
+    display: none;
+  }  
+  
   </style>
   
--- backend/vyhladajBE/resources/js/components/NavBar.vue
+++ backend/vyhladajBE/resources/js/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-[#121212]">
-    <div class="flex items-center w-[28.75rem] justify-between h-[70px] ">
+    <div class="flex items-center w-2/5 justify-between h-[70px] ">
       <router-link to="/" class="text-[#F8E7F8]">
         <img class="w-52 ml-16" src="@/components/nachoverse-logo.png" alt="">
       </router-link>
