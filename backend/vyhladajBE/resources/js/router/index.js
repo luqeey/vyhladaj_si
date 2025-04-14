aaa683eee802e0c2cc2c4ab6ae008b304a1719ba
Jakub <kubkozak@gmail.com>
2025-04-14 09:55:38 +0200

    fix
    prepinanie medzi series a movies a fixnutie otvaranie detailu filmov

--- backend/vyhladajBE/resources/js/router/index.js
+++ backend/vyhladajBE/resources/js/router/index.js
@@ -34,7 +34,8 @@ const router = createRouter({
             path: '/detail-movie/:id',
             name: 'detail movie',
             component: DetailMovie,
-        },{
+        },
+        {
             path: '/detail-tv-series/:id',
             name: 'detail series',
             component: DetailSeries,
